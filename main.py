b974b3aee3bf857f4357c9dbd8cd2941adb227c4
Brian Aubry <bdaubry@gmail.com>
2018-11-04 13:40:40 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy
+import cgi
+
+app = Flask(__name__)
+app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flicklist:MyNewPass@localhost:8889/flicklist'
+app.config['SQLALCHEMY_ECHO'] = True
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return 
+
+if __name__ == "__main__":
+    app.run()
