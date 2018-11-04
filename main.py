5c86c9ecd4b07fd9df2c7ef72095b4d694be79e7
Brian Aubry <bdaubry@gmail.com>
2018-11-04 16:10:17 -0600

    showing posts works now, as does adding them to the database, need to add error checking

--- main.py
+++ main.py
@@ -3,15 +3,56 @@ from flask_sqlalchemy import SQLAlchemy
 import cgi
 
 app = Flask(__name__)
-app.config['DEBUG'] = True      # displays runtime errors in the browser, too
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flicklist:MyNewPass@localhost:8889/flicklist'
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blogger@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
 
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), unique=True) 
+    body = db.Column(db.Text(), unique=False)
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
 @app.route('/')
+def reroute():
+    return redirect('/blog')
+
+@app.route('/blog', methods=['GET'])
 def index():
-    return 
+
+    posts = Blog.query.all()
+    blogid = request.args.get('id')
+
+    if blogid != None:
+        blogpost = Blog.query.filter_by(id=blogid).first()
+        print(blogpost.title)
+        print(blogpost.body)
+        return render_template('blog.html',blogid=blogpost.id, title=blogpost.title, body=blogpost.body)
+
+    
+    return render_template('blog.html', posts=posts, title="Build-A-Blog")
+
+@app.route('/newpost', methods=['GET', 'POST'])
+def newpost():
+    if request.method == 'POST':
+        #pull blog information from form, save in variables
+        title = request.form['title']
+        body = request.form['body']
+        #create a new post using the Blog class
+        newpost = Blog(title, body)
+        #add the blog post to the database, which needs title and body
+        db.session.add(newpost)
+        db.session.commit()
+
+        return redirect('/blog')
+    
+    return render_template('newpost.html', title="New Post")
+
 
 if __name__ == "__main__":
     app.run()
