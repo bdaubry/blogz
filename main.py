323019ce9e34f44a1400828e974248d08ffa4a06
Brian Aubry <bdaubry@gmail.com>
2018-12-01 23:29:50 -0600

    css working, login/logout/signup working, post filter by user working

--- main.py
+++ main.py
@@ -5,72 +5,70 @@ import cgi
 from app import app, db  
 from models import User, Blog
 
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('login.html')
     elif request.method == 'POST':
-        email = request.form['email']
+        username = request.form['username']
         password = request.form['password']
-        users = User.query.filter_by(email=email)
+        users = User.query.filter_by(username=username)
         if users.count() == 1:
             user = users.first()
             if password == user.password:
-                session['user'] = user.email
-                flash('welcome back, '+user.email)
-                return redirect("/")
-        flash('bad username or password')
+                session['user'] = user.username
+                flash('welcome back, '+user.username)
+                return redirect("/newpost")
+            else:
+                flash('bad password')
+        else:
+            flash('bad username')
         return redirect("/login")
 
-@app.route("/register", methods=['GET', 'POST'])
-def register():
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
     if request.method == 'POST':
-        email = request.form['email']
+        username = request.form['username']
         password = request.form['password']
         verify = request.form['verify']
-        if not is_email(email):
-            flash('zoiks! "' + email + '" does not seem like an email address')
-            return redirect('/register')
-        email_db_count = User.query.filter_by(email=email).count()
-        if email_db_count > 0:
-            flash('yikes! "' + email + '" is already taken and password reminders are not implemented')
-            return redirect('/register')
+        if username == '' or password == '' or verify == '':
+            flash('no field on this page can be blank')
+            return redirect('/signup')
+        user_db_count = User.query.filter_by(username=username).count()
+        if user_db_count > 0:
+            flash(username + ' is already a registered user')
+            return redirect('/signup')
         if password != verify:
             flash('passwords did not match')
-            return redirect('/register')
-        user = User(email=email, password=password)
+            return redirect('/signup')
+        if len(password) < 4:
+            flash('password is too short')
+            return redirect('/signup')
+        if len(username) < 4:
+            flash('username is too short')
+            return redirect('/signup')
+        user = User(username=username, password=password)
         db.session.add(user)
         db.session.commit()
-        session['user'] = user.email
+        session['user'] = user.username
         return redirect("/")
     else:
-        return render_template('register.html')
-
-def is_email(string):
-    # for our purposes, an email string has an '@' followed by a '.'
-    # there is an embedded language called 'regular expression' that would crunch this implementation down
-    # to a one-liner, but we'll keep it simple:
-    atsign_index = string.find('@')
-    atsign_present = atsign_index >= 0
-    if not atsign_present:
-        return False
-    else:
-        domain_dot_index = string.find('.', atsign_index)
-        domain_dot_present = domain_dot_index >= 0
-        return domain_dot_present
+        return render_template('signup.html')
 
 @app.route("/logout", methods=['POST'])
 def logout():
     del session['user']
-    return redirect("/")
+    return redirect("/blog")
 
 
 @app.route('/blog', methods=['GET', 'POST'])
-def index():
+def blog():
     posts = Blog.query.order_by('id DESC').all()
     blogid = request.args.get('id')
+    userid = request.args.get('userid')
 
-    if blogid != None:
+    if blogid: #!= None:
         blogpost = Blog.query.filter_by(id=blogid).first()
         #if request.method == 'POST':
         #    blogpost.title == new_title
@@ -78,10 +76,13 @@ def index():
 
         print(blogpost.title)
         print(blogpost.body)
-        return render_template('blog.html',blogid=blogpost.id, title=blogpost.title, body=blogpost.body, date=blogpost.date)
+        return render_template('blog.html',blogid=blogpost.id, title=blogpost.title, body=blogpost.body, date=blogpost.date, owner=blogpost.owner.username)
 
-        
-
+    if userid: #!= None:
+        blogs = Blog.query.filter_by(owner_id=userid).all()
+        user = User.query.filter_by(id=userid).first()
+        return render_template('blog.html', posts=blogs, pagetitle=user.username+"'s Posts")
+        #return render_template('blog.html', blogid=blogs.id, title=blogs.title, body=blogs.body, date=blogs.date, owner=blogs.owner.username)
 
     return render_template('blog.html', posts=posts, pagetitle="Build-A-Blog")
 
@@ -100,7 +101,7 @@ def newpost():
             error = "Body can't be blank"
             return render_template('newpost.html', title=title, error_msg=error)
         #create a new post using the Blog class
-        newpost = Blog(title, body)
+        newpost = Blog(title, body, logged_in_user())
         #add the blog post to the database, which needs title and body
         db.session.add(newpost)
         db.session.commit()
@@ -109,20 +110,26 @@ def newpost():
     
     return render_template('newpost.html', pagetitle="New Post")
 
+
 @app.route('/')
-def reroute():
-    return redirect('/blog')
+def index():
+    users = User.query.order_by('username DESC').all()
+    return render_template('index.html', users=users)
+
+# def reroute():
+#     return redirect('/blog')
 
 def logged_in_user():
-    owner = User.query.filter_by(email=session['user']).first()
-    return owner.id
+    owner = User.query.filter_by(username=session['user']).first()
+    return owner
 
-endpoints_without_login = ['login', 'register']
+endpoints_without_login = ['index', 'login', 'signup', 'reroute', 'blog', 'static']
 
 @app.before_request
 def require_login():
     if not ('user' in session or request.endpoint in endpoints_without_login):
-        return redirect("/login")
+        flash('you must be logged in to complete that action')
+        return redirect('/login')
 
 app.secret_key = 'WOWweEEILoFELauncerCODERItsMYfavorIETCLase'
 
--- models.py
+++ models.py
@@ -1,26 +1,28 @@
 from app import db
+from sqlalchemy.sql import func
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+    
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
+    def __repr__(self):
+        return self.username
+        # return '<User %r>' % self.username
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), unique=False) 
     body = db.Column(db.Text(), unique=False)
     date = db.Column(db.DateTime, unique=False)
-    owner = db.Column(db.Integer, unique=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body, owner_id):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
         self.date = func.now()
-        self.owner_id = owner_id
+        self.owner = owner
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120))
-    
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password
-
-    def __repr__(self):
-        return '<User %r>' % self.email
